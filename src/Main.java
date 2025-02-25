7c0c6f6e8ce58c51992bff9d4b84714eb0f3e839
Lenar Khafizov <lenarkhaf@gmail.com>
2025-02-25 19:46:37 +0300

    Выполнено задание 2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100; // сумма средств на счету
+        int paySum = 1110; // сумма пополнения
+        int bonus;         // величина бонуса
+        if (paySum > 1000) {
+            bonus = paySum / 100;
+        } else
+        {
+            bonus = 0;
+        }
+        balance = balance + paySum + bonus;
+        System.out.println("Вам начислен бонус в сумме " + bonus + " руб.");
+        System.out.println("Баланс Вашего лицевого счета " + balance + " руб.");
+    }
+}
